84720b5c1f186028f5b473dc1e355205741e358d
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-06-17 09:39:05 +0200

    init

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,179 +1,197 @@
 var siteTheme = gbifReactComponents.themeBuilder.extend({
-  baseTheme: 'light', extendWith: {
-      primary: themeStyle.colors.primary
-  }
+    baseTheme: 'light', extendWith: {
+        primary: themeStyle.colors.primary
+    }
 });
 
-var publisherKeys = [
-  '760d5f24-4c04-40da-9646-1b2c935da502',
-  '2e7df380-8356-4533-bcb3-5459e23c794e',
-  '8e1a97a0-3ca8-11d9-8439-b8a03c50a862',
+var datasetKeys = [
+    "1fe5d5cd-0336-4664-b768-d38acf349c0a",
+    "97367d97-2b0c-4895-aee7-070f9bb42e5a",
+    "3bef6fb2-9a1f-4d8d-b5f3-d6d23885905c",
+    "e51b8857-2ad1-4701-a276-f0cd1f4cc6a5",
+    "2a337d5a-a784-4f8a-a947-46d69e0fa3eb",
+    "2618e878-7e19-412d-99e3-f12a8aa23ebb",
+    "94dce9c1-e2f0-45cb-a77b-8e5caa871a41",
+    "75bcbf8f-5e58-4714-9eba-382129702022",
+    "cf8f886b-7289-4d59-9cb1-f2d293147d98",
+    "bc2c3c27-97b6-473e-abe0-6f8478b3e2f8",
+    "9c2d26f1-0ce0-4cde-9f8d-de8284404fff",
+    "94687f6b-6723-46a3-90d8-5a06b843291c"
 ];
 
 var institutionKeys = [
-  '68fcaba0-61fe-4e78-a460-4f37654d025d',
-  '59f46093-8fae-47f3-a9ef-e5fd1d38e4fe',
-  '44e7a331-0270-4c39-ad64-91c0d8416480',
+    'c8600560-0b5f-4e49-abf0-4812643c47b4',
 ];
 
 var siteConfig = {
-  "version": 3,
-  "pages": [
-      {
-          "id": "occurrenceSearch"
-      },
-      {
-          "id": "collectionSearch"
-      },
-      {
-          "id": "collectionKey"
-      },
-      {
-          "id": "datasetSearch"
-      },
-      {
-          "id": "datasetKey"
-      },
-      {
-          "id": "institutionKey"
-      },
-      {
-          "id": "literatureSearch"
-      }
-  ],
-  "disableInlineTableFilterButtons": false,
-  "availableCatalogues": [
-      "OCCURRENCE",
-      "DATASET",
-      "COLLECTION",
-      "LITERATURE"
-  ],
-  "dataHeader": {
-      "enableApiPopup": false,
-      "enableInfoPopup": false
-  },
-  "theme": {
-      "primary": "#001972",
-      "borderRadius": 3,
-      "stickyOffset": "0px"
-  },
-  "maps": {
-      "mapStyles": {
-          "defaultProjection": "MERCATOR",
-          "defaultMapStyle": "BRIGHT",
-          "options": {
-              "MERCATOR": [
-                  "BRIGHT",
-                  "NATURAL"
-              ]
-          }
-      }
-  },
-  "languages": [
-      {
-          "code": "en",
-          "localeCode": "en",
-          "label": "English",
-          "default": true,
-          "textDirection": "ltr",
-          "iso3LetterCode": "eng",
-          "cmsLocale": "en-GB",
-          "gbifOrgLocalePrefix": "",
-          "mapTileLocale": "en"
-      },
-      {
-          "code": "da",
-          "localeCode": "da",
-          "label": "Dansk",
-          "default": false,
-          "textDirection": "ltr",
-          "iso3LetterCode": "dan",
-          "cmsLocale": "en-GB",
-          "gbifOrgLocalePrefix": "",
-          "mapTileLocale": "en"
-      }
-  ],
-  "messages": {},
-  "occurrenceSearch": {
-      "scope": {
-          "type": "in",
-          "key": "publishingOrg",
-          "values": [
-              "760d5f24-4c04-40da-9646-1b2c935da502",
-              "2e7df380-8356-4533-bcb3-5459e23c794e",
-              "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-          ]
-      },
-      "highlightedFilters": [
-          "taxonKey",
-          "verbatimScientificName",
-          "institutionKey",
-          "collectionKey",
-          "catalogNumber",
-          "recordedBy",
-          "identifiedBy"
-      ],
-      "excludedFilters": [
-          "occurrenceStatus",
-          "networkKey",
-          "hostingOrganizationKey",
-          "protocol",
-          "publishingCountry",
-          "institutionCode",
-          "collectionCode"
-      ],
-      "defaultEnabledTableColumns": [
-          "features",
-          "institutionKey",
-          "collectionKey",
-          "catalogNumber",
-          "country",
-          "year",
-          "recordedBy",
-          "identifiedBy"
-      ],
-      "tabs": [
-          "table",
-          "gallery",
-          "map",
-          "clusters",
-          "dashboard",
-          "download"
-      ],
-      "mapSettings": {
-          "lat": 52,
-          "lng": 12,
-          "zoom": 4.911544076366507
-      }
-  },
-  "collectionSearch": {
-      excludedFilters: ['country', 'active'],
-      // highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
-      // defaultTableColumns: ['title', 'description', 'publisher', 'type', 'occurrenceCount', 'literatureCount'],
-      scope: {
-          institutionKey: institutionKeys,
-          active: true
-      },
-  },
-  "institutionSearch": {},
-  "datasetSearch": {
-      excludedFilters: ['publishingCountry', 'networkKey', 'projectId', 'hostingOrg'],
-      highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
-      // defaultTableColumns: ['title', 'description', 'publisher', 'type', 'occurrenceCount', 'literatureCount'],
-      scope: {
-          publishingOrg: publisherKeys
-      },
-  },
-  "publisherSearch": {},
-  "literatureSearch": {
-      "scope": {
-          "type": "in",
-          "key": "publishingOrganizationKey",
-          "values": [
-              "760d5f24-4c04-40da-9646-1b2c935da502",
-              "2e7df380-8356-4533-bcb3-5459e23c794e",
-              "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-          ]
-      }
-  }
+    "version": 3,
+    "pages": [
+        {
+            "id": "occurrenceSearch"
+        },
+        {
+            "id": "collectionSearch"
+        },
+        {
+            "id": "collectionKey"
+        },
+        {
+            "id": "datasetKey"
+        },
+        {
+            "id": "literatureSearch"
+        }
+    ],
+    "disableInlineTableFilterButtons": false,
+    "availableCatalogues": [
+        "OCCURRENCE",
+        "DATASET",
+        "COLLECTION",
+        "LITERATURE"
+    ],
+    "dataHeader": {
+        "enableApiPopup": false,
+        "enableInfoPopup": false
+    },
+    "theme": {
+        "primary": "#001972",
+        "borderRadius": 3,
+        "stickyOffset": "0px"
+    },
+    "maps": {
+        "mapStyles": {
+            "defaultProjection": "MERCATOR",
+            "defaultMapStyle": "BRIGHT",
+            "options": {
+                "MERCATOR": [
+                    "BRIGHT",
+                    "NATURAL"
+                ]
+            }
+        }
+    },
+    "languages": [
+        {
+            "code": "en",
+            "localeCode": "en",
+            "label": "English",
+            "default": true,
+            "textDirection": "ltr",
+            "iso3LetterCode": "eng",
+            "cmsLocale": "en-GB",
+            "gbifOrgLocalePrefix": "",
+            "mapTileLocale": "en"
+        },
+        {
+            "code": "da",
+            "localeCode": "da",
+            "label": "Dansk",
+            "default": false,
+            "textDirection": "ltr",
+            "iso3LetterCode": "dan",
+            "cmsLocale": "en-GB",
+            "gbifOrgLocalePrefix": "",
+            "mapTileLocale": "en"
+        }
+    ],
+    "messages": {},
+    "occurrenceSearch": {
+        "scope": {
+            "type": "in",
+            "key": "datasetKey",
+            "values": datasetKeys
+        },
+        "highlightedFilters": [
+            "taxonKey",
+            "verbatimScientificName",
+            "institutionKey",
+            "collectionKey",
+            "catalogNumber",
+            "recordedBy",
+            "identifiedBy"
+        ],
+        "excludedFilters": [
+            "occurrenceStatus",
+            "networkKey",
+            "hostingOrganizationKey",
+            "protocol",
+            "publishingCountry",
+            "publishingOrg",
+            "institutionCode",
+            "collectionCode"
+        ],
+        "defaultEnabledTableColumns": [
+            "features",
+            "institutionKey",
+            "collectionKey",
+            "catalogNumber",
+            "country",
+            "year",
+            "recordedBy",
+            "identifiedBy"
+        ],
+        "availableTableColumns": [
+            "scientificName",
+            "commonName",
+            "features",
+            "media",
+            "country",
+            "coordinates",
+            "year",
+            "eventDate",
+            "basisOfRecord",
+            "dataset",
+            "catalogNumber",
+            "recordedBy",
+            "identifiedBy",
+            "recordNumber",
+            "typeStatus",
+            "preparations",
+            "collectionCode",
+            "collectionKey",
+            "locality",
+            "fieldNumber",
+            "individualCount",
+            "higherGeography",
+            "stateProvince",
+            "establishmentMeans",
+            "sex",
+            "lifeStage",
+            "iucnRedListCategory"
+        ],
+        "tabs": [
+            "table",
+            "gallery",
+            "map",
+            "clusters",
+            "dashboard",
+            "download"
+        ],
+        "mapSettings": {
+            "lat": 52,
+            "lng": 12,
+            "zoom": 4.911544076366507
+        }
+    },
+    "collectionSearch": {
+        excludedFilters: ['country', 'active'],
+        // highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
+        // defaultTableColumns: ['title', 'description', 'publisher', 'type', 'occurrenceCount', 'literatureCount'],
+        scope: {
+            institutionKey: institutionKeys,
+            active: true
+        },
+    },
+    "institutionSearch": {},
+    "datasetSearch": {
+
+    },
+    "publisherSearch": {},
+    "literatureSearch": {
+        "scope": {
+            "type": "in",
+            "key": "gbifDatasetKey",
+            "values": datasetKeys
+        }
+    }
 }
--- en/home.md
+++ en/home.md
@@ -1,10 +1,10 @@
 ---
 lang-ref: home
 layout: home
-title: Danish System of Scientific Collections
-description: Part of the European Project DiSSCo
-background:  "{{ site.data.images.shells_with_barcode.src }}"
-imageLicense: "{{ site.data.images.shells_with_barcode.caption }}"
+title: Humboldt Collections
+description: Lorem markdownum spatium limes indefessus neque *at* orat aestuat.
+background:  "{{ site.data.images.catocala_ilia_cramer.src }}"
+imageLicense: "{{ site.data.images.catocala_ilia_cramer.caption }}"
 height: 90vh
 cta:
   - text: Specimens
